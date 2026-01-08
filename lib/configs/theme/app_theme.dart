5f03486e1c5e3bd9d992344a45e13ca130806dd0
WafaaSisalem <wafaasesalem@gmail.com>
2026-01-08 11:49:57 +0300

    delete packages and unnecessary platforms

--- lib/configs/theme/app_theme.dart
+++ lib/configs/theme/app_theme.dart
@@ -11,7 +11,7 @@ class AppTheme {
     return ThemeData(
       useMaterial3: true,
       fontFamily: 'Poppins',
-      
+
       // Color Scheme
       colorScheme: ColorScheme.light(
         primary: AppColors.mainColor,
@@ -43,17 +43,29 @@ class AppTheme {
         displayLarge: AppTypography.h1_64Bold.copyWith(color: AppColors.black),
         displayMedium: AppTypography.h1_56Bold.copyWith(color: AppColors.black),
         displaySmall: AppTypography.h1_48Bold.copyWith(color: AppColors.black),
-        headlineLarge: AppTypography.h2_48SemiBold.copyWith(color: AppColors.titles),
-        headlineMedium: AppTypography.h2_36SemiBold.copyWith(color: AppColors.titles),
-        headlineSmall: AppTypography.h3_36Medium.copyWith(color: AppColors.titles),
+        headlineLarge: AppTypography.h2_48SemiBold.copyWith(
+          color: AppColors.titles,
+        ),
+        headlineMedium: AppTypography.h2_36SemiBold.copyWith(
+          color: AppColors.titles,
+        ),
+        headlineSmall: AppTypography.h3_36Medium.copyWith(
+          color: AppColors.titles,
+        ),
         titleLarge: AppTypography.h4_28Medium.copyWith(color: AppColors.titles),
-        titleMedium: AppTypography.h5_20Medium.copyWith(color: AppColors.titles),
+        titleMedium: AppTypography.h5_20Medium.copyWith(
+          color: AppColors.titles,
+        ),
         titleSmall: AppTypography.h5_16Medium.copyWith(color: AppColors.titles),
         bodyLarge: AppTypography.body18Regular.copyWith(color: AppColors.text),
         bodyMedium: AppTypography.body16Regular.copyWith(color: AppColors.text),
         bodySmall: AppTypography.body14Regular.copyWith(color: AppColors.text),
-        labelLarge: AppTypography.label12Regular.copyWith(color: AppColors.text),
-        labelMedium: AppTypography.label10Regular.copyWith(color: AppColors.text),
+        labelLarge: AppTypography.label12Regular.copyWith(
+          color: AppColors.text,
+        ),
+        labelMedium: AppTypography.label10Regular.copyWith(
+          color: AppColors.text,
+        ),
       ),
 
       // Input Decoration Theme
@@ -63,9 +75,7 @@ class AppTheme {
         hintStyle: AppTypography.body16Regular.copyWith(
           color: AppColors.placeholders,
         ),
-        labelStyle: AppTypography.body14Regular.copyWith(
-          color: AppColors.text,
-        ),
+        labelStyle: AppTypography.body14Regular.copyWith(color: AppColors.text),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8),
           borderSide: BorderSide(color: AppColors.border),
@@ -82,7 +92,10 @@ class AppTheme {
           borderRadius: BorderRadius.circular(8),
           borderSide: BorderSide(color: AppColors.error),
         ),
-        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 16,
+        ),
       ),
 
       // Elevated Button Theme
@@ -92,9 +105,7 @@ class AppTheme {
           foregroundColor: AppColors.white,
           elevation: 0,
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           textStyle: AppTypography.buttonBig,
         ),
       ),
@@ -114,29 +125,22 @@ class AppTheme {
           foregroundColor: AppColors.mainColor,
           side: BorderSide(color: AppColors.mainColor),
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           textStyle: AppTypography.buttonBig,
         ),
       ),
 
       // Card Theme
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color: AppColors.white,
         elevation: 0,
         shadowColor: Colors.black.withOpacity(0.1),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         margin: const EdgeInsets.all(8),
       ),
 
       // Icon Theme
-      iconTheme: IconThemeData(
-        color: AppColors.icons,
-        size: 24,
-      ),
+      iconTheme: IconThemeData(color: AppColors.icons, size: 24),
 
       // Divider Theme
       dividerTheme: DividerThemeData(
