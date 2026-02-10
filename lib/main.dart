933ea2bc59e56676bfb4f2f35a02e8581d5b5716
amal <amalananana@gmail.com>
2026-02-10 12:40:26 +0200

    fixing main and app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:second_hand_electronics_marketplace/features/chating/presentation/screens/all_chats_screen.dart';
-import 'package:second_hand_electronics_marketplace/features/chating/presentation/screens/chating_screen.dart';
 import 'imports.dart';
 void main() {
   runApp(
     DevicePreview(enabled: true, builder: (context) => const
     ElectroLinkApp()
-    // ChatsScreen(),
-    // ListingsScreen(title: 'Listings', listings: []),
     ),
   );
 }
