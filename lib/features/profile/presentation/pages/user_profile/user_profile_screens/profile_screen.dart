7d5d1bd01599f9c3a49601a2300ba1573493347c
amal <amalananana@gmail.com>
2026-02-27 00:24:56 +0200

    profile connected to API

--- lib/features/profile/presentation/pages/user_profile/user_profile_screens/profile_screen.dart
+++ lib/features/profile/presentation/pages/user_profile/user_profile_screens/profile_screen.dart
@@ -11,16 +11,16 @@ import '../../../widgets/profile_widgets_exports.dart';
 class ProfileScreen extends StatelessWidget {
   final bool isMe;
   final UserModel authUser;
+
   const ProfileScreen({super.key, required this.authUser, required this.isMe});
 
   @override
   Widget build(BuildContext context) {
-
     return BlocProvider(
-      create: (context) => ProfileBloc(
-        context.read<ProfileService>(), // ProfileService موجود في MultiBlocProvider بالمين
-        authUser,
-      )..add(FetchProfileEvent(isMe: isMe)), // مباشرةً نجيب البروفايل من API
+      create:
+          (context) =>
+              ProfileBloc(context.read<ProfileService>(), authUser)
+                ..add(FetchProfileEvent(isMe: isMe)),
       child: BlocListener<ProfileBloc, ProfileState>(
         listener: (context, state) {
           if (state is ProfileLoading) {
@@ -38,56 +38,66 @@ class ProfileScreen extends StatelessWidget {
                 type: state.isMe ? ProfileType.private : ProfileType.public,
               );
 
-              final verificationProgress = state.appUser.user.verificationPercentage / 100;
-              final userListings = dummyListings
-                  .where((l) => l.ownerId == state.appUser.user.id.toString()) // إذا ownerId String
-                  .toList();
-               return Scaffold(
-                  appBar: AppBar(
-                    title: Text(profileData.name),
-                    actions: [ProfileAppBarActions(isMe: state.isMe)],
+              final verificationProgress =
+                  state.appUser.user.verificationPercentage / 100;
+              final userListings =
+                  dummyListings
+                      .where(
+                        (l) => l.ownerId == state.appUser.user.id.toString(),
+                      )
+                      .toList();
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(profileData.name),
+                  actions: [ProfileAppBarActions(isMe: state.isMe)],
+                ),
+                body: SingleChildScrollView(
+                  padding: const EdgeInsets.fromLTRB(
+                    AppSizes.paddingM,
+                    AppSizes.paddingM,
+                    AppSizes.paddingM,
+                    AppSizes.paddingL,
                   ),
-                  body: SingleChildScrollView(
-                    padding: const EdgeInsets.fromLTRB(
-                      AppSizes.paddingM,
-                      AppSizes.paddingM,
-                      AppSizes.paddingM,
-                      AppSizes.paddingL,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        ProfileHeader(
-                          profile: profileData,
-                          type:
-                              state.isMe
-                                  ? ProfileType.private
-                                  : ProfileType.public,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ProfileHeader(
+                        profile: profileData,
+                        type:
+                            state.isMe
+                                ? ProfileType.private
+                                : ProfileType.public,
+                      ),
+                      SizedBox(height: AppSizes.paddingS),
+                      if (state.isMe)
+                        PrivateProfileCompletion(
+                          verificationProgress: verificationProgress,
                         ),
-                        SizedBox(height: AppSizes.paddingS),
-                        if (state.isMe)
-                          PrivateProfileCompletion(
-                            verificationProgress: verificationProgress,
+                      SizedBox(
+                        height:
+                            state.isMe
+                                ? AppSizes.paddingM
+                                : AppSizes.paddingXXS,
+                      ),
+                      state.isMe
+                          ? PrivateProfileWidget(
+                            userListings: userListings,
+                            user: authUser,
+                          )
+                          : PublicProfileWidget(
+                            userListings: userListings,
+                            user: authUser,
                           ),
-                        SizedBox(
-                          height:
-                              state.isMe
-                                  ? AppSizes.paddingM
-                                  : AppSizes.paddingXXS,
-                        ),
-                        state.isMe
-                            ? PrivateProfileWidget(userListings: userListings,user:authUser ,)
-                            : PublicProfileWidget(userListings: userListings,user: authUser,),
-                      ],
-                    ),
+                    ],
                   ),
-                );
-              }
-              return const SizedBox();
-            },
-          ),
+                ),
+              );
+            }
+            return const SizedBox.shrink();
+          },
         ),
+      ),
     );
   }
 }
